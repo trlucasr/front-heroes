270cbf60b34611fee014b0705179bc634a4d15ca
Lincoln Ferreira <lincolnferreira2012@gmail.com>
2025-03-24 22:31:35 -0300

    Alteração main
    Adicionado função para limpar dados da busca da api, e adicionado url do axios ao fastapi para o cors (que resolve problema do fastapi permitir o axios se conectar)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
 
 app = FastAPI()
+
+origins = ["http://localhost:5500"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def limpar_dados_busca(dados_sujos):
+    resultados = dados_sujos.get('results')
+    dados_limpos = []
+    for heroi in resultados:
+        nome = heroi["biography"]["full-name"]
+        alter_ego = heroi["name"]
+        img = heroi["image"]["url"]
+        dados_limpos.append({"nome" : nome, "alter-ego" : alter_ego, "img" : img})
+    return dados_limpos
+
 
 @app.get("/")
 async def home():
@@ -27,7 +49,8 @@ def buscar_heroi(nome : str):
     resposta = requests.get(url)
 
     if resposta.status_code == 200:
-        dados_heroi = resposta.json()
-        return dados_heroi
+        dados = resposta.json()
+        dados_limpos = limpar_dados_busca(dados)
+        return dados_limpos
     else:
         return {"Erro:": "Não foi possível achar o herói"} 
