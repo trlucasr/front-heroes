b14eadf4a99aeabdbf8b7c7ddd9b209255a232a6
Lincoln Ferreira <lincolnferreira2012@gmail.com>
2025-03-27 16:53:03 -0300

    Implementação banco herois json
    Implementação de função que busca herois na api e salva os dados relevantes de todos os heróis em um json.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import requests, json
+import requests, json, os
 from webscrapping import BS
 
 app = FastAPI()
 
+chave_api = "4315a5181d6f31a64e55172efb01bc64"
+
 origins = ["http://localhost:5500"
            ,"http://127.0.0.1:5500"
-           ] #porta do liveserver / apenas localhost não funcionou
+           ] #porta do liveserver / apenas localhost com porta não funcionou
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,9 +29,9 @@ def limpar_dados_busca(dados_sujos):
         dados_limpos.append({"nome" : nome, "alterego" : alter_ego, "img" : img})
     return dados_limpos
 
-def limpar_dados_id(dados_sujos):
-    try: alter_ego = dados_sujos["name"]
-    except: alter_ego = ""
+def limpar_dados_id(dados_sujos, id_heroi):
+    try: alterego = dados_sujos["name"]
+    except: alterego = ""
 
     try: img = dados_sujos["image"]["url"]
     except: img = ""
@@ -37,7 +39,7 @@ def limpar_dados_id(dados_sujos):
     try: nome = dados_sujos.get("biography")["full-name"]
     except: nome = ""
 
-    dados_limpos = {"nome" : nome, "alterego" : alter_ego, "img" : img}
+    dados_limpos = {"id" : id_heroi, "nome" : nome, "alterego" : alterego, "img" : img}
     return dados_limpos
 
 def salvar_ultimo_heroi(id):
@@ -45,8 +47,45 @@ def salvar_ultimo_heroi(id):
         arquivo.write(str(id))
 
 def ultimo_heroi_salvo():
+    if not os.path.exists("./ultimo-heroi.json"):
+        print(f"Erro: O arquivo não existe.")
+        return 0
+    
     with open("ultimo-heroi.json", "r") as arquivo:
-        return int(arquivo.read())
+        numero = json.load(arquivo)
+    
+    if not isinstance(numero, (int, float)):
+            print("Erro: O arquivo não contém um número válido.")
+            return 0
+    
+    return numero
+    
+def atualizar_banco_herois(qtd_herois):
+    herois = []
+    for id in range(1, qtd_herois+1):
+        url = f"https://superheroapi.com/api/{chave_api}/{id}"
+        try:
+            resposta = requests.get(url)
+            if resposta.status_code == 200:
+                dados = resposta.json()
+                dados_limpos = limpar_dados_id(dados, id)
+                herois.append(dados_limpos)
+            else: raise ValueError(f"status code: {resposta.status_code}")
+        except Exception as ex1:
+            print(f"Erro ao fazer o request do id {id} na função registrar_herois.\nErro: {ex1}\nExecutando segunda tentativa")
+            try:
+                resposta = requests.get(url)
+                if resposta.status_code == 200:
+                    dados = resposta.json()
+                    dados_limpos = limpar_dados_id(dados, id)
+                    herois.append(dados_limpos)
+                    print(f"A segunda tentativa de coletar o heroi {id} foi bem sucedida.")
+                else: raise ValueError(f"status code: {resposta.status_code}")
+            except Exception as ex2:
+                print(f"Erro ao executar a segunda tentativa no request do id {id}.\nErro: {ex2}")
+
+    with open("herois.json", "w") as arquivo:
+        json.dump(herois, arquivo, ensure_ascii=False, indent=4)
 
 @app.get("/")
 async def home():
@@ -54,7 +93,7 @@ async def home():
 
 @app.get("/buscar")
 def buscar_heroi(nome : str):
-    url = f"https://superheroapi.com/api/4315a5181d6f31a64e55172efb01bc64/search/{nome}"
+    url = f"https://superheroapi.com/api/{chave_api}/search/{nome}"
     resposta = requests.get(url)
 
     if resposta.status_code == 200:
@@ -66,20 +105,9 @@ def buscar_heroi(nome : str):
     
 @app.get("/atualizar-banco")
 def atualizar_banco():
-    #url = "https://superheroapi.com/ids.html"
-    #ultimo_heroi_api = BS.verificar_ultimo_heroi(url)
-    #if ultimo_heroi_api > ultimo_heroi_salvo():
-    #    salvar_ultimo_heroi(ultimo_heroi_api)
-    ultimo_heroi = ultimo_heroi_salvo()
-    
-    herois = []
-    for i in range(ultimo_heroi):
-        url = f"https://superheroapi.com/api/4315a5181d6f31a64e55172efb01bc64/{i}"
-        resposta = requests.get(url)
-        #if resposta.status_code == 200:
-        #    dados = resposta.json()
-        #    dados_limpos = limpar_dados_id(dados)
-        #    herois.append(dados_limpos)
-        herois.append(resposta.json())
-    
-    return herois
+    url = "https://superheroapi.com/ids.html"
+    qtd_heroi_api = BS.verificar_ultimo_heroi(url)
+    if qtd_heroi_api > ultimo_heroi_salvo():
+        salvar_ultimo_heroi(qtd_heroi_api)
+    qtd_herois = ultimo_heroi_salvo()
+    atualizar_banco_herois(qtd_herois)
